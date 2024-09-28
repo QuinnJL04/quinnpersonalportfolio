9b796051711113116c0c87742800eac6ebfe299b
QuinnJL04 <quinnlouie04@gmail.com>
2024-09-28 15:41:33 -0400

    a whole lotta stuff :)

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ export const Hero = () => {
     return <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Quinn</h1>
-            <p className={styles.description}>I'm a 3rd year CS + AI/ML Concentration student at Noreastern Unithversity. I have 6 months of co-op experience working with C#, Node.js, and SQL, though my strongest languages are Java, Python, and SQL. Reach out if you'd like to learn more!</p>
+            <p className={styles.description}>I’m a 3rd-year Computer Science student at Northeastern with a focus on AI/ML. I’ve spent six months as a co-op working with C#, Node.js, and SQL, though Java, Python, and SQL are where I’m most at home. I like to keep things practical and straightforward—unless it’s deciding what to order for lunch. Feel free to reach out if you want to chat or work together!</p>
             <a href="mailto:myemail@email.com" className={styles.contactBtn}>Contact Me</a>
         </div>
         <img src={getImageUrl("hero/quinnn.jpg")} alt="Hero image of me" className={styles.heroImg}/>
